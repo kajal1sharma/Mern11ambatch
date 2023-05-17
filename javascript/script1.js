037b955cf45ea914fd34c6869fc40d945272649d
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-17 11:28:03 +0530

    ..

---
+++ javascript/script1.js
@@ -0,0 +1,60 @@
+
+let numberOne = 23 ;
+let numberTwo = 45;
+
+
+// console.log(34==='34');// types and value(strict type checking)
+//console.log(34=='34');//value =>implicit conversion
+
+// console.log(45!=='45');
+// console.log(45!='45')
+
+// console.log(numberOne<numberTwo);
+// console.log(34>45);
+// console.log(34<=45);
+// console.log(34<=34);
+// console.log(45>=36);
+// console.log(34==34);
+
+
+// console.log(numberOne + numberTwo)
+// console.log(numberTwo -numberOne);
+// console.log(numberTwo/numberOne);
+// console.log(numberOne*numberTwo)
+// console.log(numberTwo%numberOne)
+
+
+
+
+
+
+// console.log("hellow worldd")
+// let firstName = "neeta"
+// let surname ="verma"
+// let fullname = firstName +" " + surname
+// console.log(fullname)
+
+
+// let scoreTeam1=45;
+// let scoreTeam2 =67;
+// //expression =>  anything that can be evaluated further
+// // 12+45+90-89
+// // 76-89
+// // prompt()
+// // block is an area covered or enclosed within curly brackets
+// //0 , null , undefined, false =false
+// if({}){
+//     console.log("congratulations Team 1");
+// }
+// else{
+//     console.log("congratulations Team 2");
+// }
+
+
+
+
+
+// console.log("heelll0o world ")
+// // alert("hellow alert fucntion")
+// let num =10;
+// num=prompt("enter some number");
