a11d36d5479af2053d880cc89e47411ebe31b897
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-22 12:24:08 +0530

    ..

---
+++ javascript/script3.js
@@ -0,0 +1,45 @@
+let day=prompt();
+switch(day){
+    case "Monday":console.log("today is Monday");
+                    let a=434+34
+                    let b=a+90;
+                    console.log(a+b);
+                    break;
+    case "Tuesday":console.log("today is Tuesday");
+            break;
+    case "Wednesday":console.log("today is Wednesday");
+            break;
+    case "Thursday":console.log("today is Thursday");
+            break
+    default :console.log("noday")
+}
+
+
+
+
+/*multi line comments
+console.log("hello world");
+console.log("hello world 2")*/
+//single line comment
+//(exp)+((exp)-((exp)*(exp)))
+
+
+//ternary operator
+//operand 1 => boolean result
+//(operand1)?(operand 2):(operand 3)
+
+// let rain=false;
+
+// console.log(rain===true);
+
+// let a =2+3
+// console.log(2+3);
+// let result = rain===true?(2+3):(5+6)
+// console.log(result);
+
+// let result = rain===true? console.log("hellow world true"): console.log("hellow world false");
+
+
+// let result =console.log("hello")
+
+// console.log("result = ",result);
