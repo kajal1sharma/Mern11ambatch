91393aca7109b33c229cb55cd2fe2fce43218298
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-23 11:49:44 +0530

    ..

---
+++ javascript/script4.js
@@ -0,0 +1,26 @@
+//console.log("hello Day 4 javaascript")
+
+
+
+// for(let a =1 ; a<=10; a=a+1){
+
+//     let str1="7 * " + a + " = " + a*7
+
+//     console.log(str1);
+
+// }
+
+
+
+
+
+// => 11+12+13+..+50
+let str ="";
+let sum=0;
+for(let a=11 ; a<=50 ;a=a+1){
+    str=str+a+"+"
+    sum=sum+a;
+}
+
+
+console.log(str+"0 = "+ sum)
