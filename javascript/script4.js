09d77160154be807c651916e7c243f8ece5e320f
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-23 12:22:29 +0530

    ..

--- javascript/script4.js
+++ javascript/script4.js
@@ -1,3 +1,18 @@
+
+//template literal 
+
+let a =90;
+let b=89;
+
+let sum=a+b;
+
+
+let str =a + "+" + b + "=" + sum
+let str2=`${a}+${b}=${sum}`
+let str3=`the sum of two numbers is ${sum}`
+console.log(str3);
+
+
 //console.log("hello Day 4 javaascript")
 
 
@@ -15,12 +30,32 @@
 
 
 // => 11+12+13+..+50
-let str ="";
-let sum=0;
-for(let a=11 ; a<=50 ;a=a+1){
-    str=str+a+"+"
-    sum=sum+a;
-}
+// let str ="";
+// let sum=0;
+// for(let a=11 ; a<=50 ;a=a+1){
+//     str=str+a+"+"
+//     sum=sum+a;
+// }
 
 
-console.log(str+"0 = "+ sum)
+// console.log(str+"0 = "+ sum)
+
+//9 even odd
+// 9 18 27 36 45 54 63 72 81 90
+// 9+27+45...81 =>odd
+// 18+36+54+...90 => even
+
+// let evenSum =0;
+// let oddSum=0
+// for( let a= 9 ; a<=90 ;a=a+9 ){
+//     if(a%2==0){
+//         evenSum= evenSum+ a;
+//     }
+//     else{
+//         oddSum=oddSum+a;
+//     }
+// }
+
+
+// console.log(evenSum," && ",oddSum)
+
