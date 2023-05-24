089dab3a06829bc3564cd74b6acaebce989e93d0
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-24 12:08:00 +0530

    ..

---
+++ javascript/script5.js
@@ -0,0 +1,98 @@
+// console.log("hello world script 5")
+
+//number 1
+// * 
+// * * 
+// * * *
+// * * * *
+// * * * * *
+
+// for(let a =0;a<5 ;a=a+1){
+// let str="";
+//     for(let b=0 ;b <=a; b=b+1){
+//          str=str + " * ";
+//     }
+// console.log(str);
+
+// }
+//number 2
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
+
+for(let a=0;a<5;a=a+1){
+    let str="";
+    for( let b=5 ; b>a; b=b-1){
+        str=str+" * ";
+    }
+    console.log(str);
+}
+
+
+// number 3
+//         *
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+
+
+for(let a =0;a<5;a=a+1){
+
+    let str="";
+    for( let b=4 ; b>a; b=b-1){
+        str=str+"   ";
+    }
+    for(let c=0 ;c<=a;c=c+1){
+        str =str+" * "
+    }
+    
+console.log(str);
+
+}
+
+
+// number 4
+//         *
+//       * * * 
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+
+
+//number 5
+* * * * * * * * * * *
+  * * * * * * * * *
+    * * * * * * *
+      * * * * *
+        * * *
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+  * * * * * * * * *
+* * * * * * * * * * *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let a =0 ; a!==5 ;  a=a+1){
+    // console.log("hellow world")
+// }
+
+
+
