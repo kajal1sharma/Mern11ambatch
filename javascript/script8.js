e733499901b8205335ab1c7f11e4bab82c491411
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-31 11:35:31 +0530

    ..

---
+++ javascript/script8.js
@@ -0,0 +1,34 @@
+//function declaration
+function print(){
+    let a=90;
+    let b=80;
+    console.log( "output of print fucntion",a+b);
+}
+
+print();
+
+
+//fucntion expression
+let str="asfdsadf";
+let read = function(){
+    let c=89;
+    let d=34;
+    console.log("output of read function",c*d);
+}
+
+read();
+
+
+//arrow function 
+
+let printIt = ()=>{
+    let r=23;
+    let m=56;
+    console.log("arrow fucntion out put is ", r-m)
+}
+
+
+printIt()
+
+
+
